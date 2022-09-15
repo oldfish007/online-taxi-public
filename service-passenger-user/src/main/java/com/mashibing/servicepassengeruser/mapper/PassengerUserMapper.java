98c71217f0cdb60c594350aa3a1f9ef9ed6b3fdf
oldfish007 <466403678@qq.com>
2022-09-15 14:26:53 +0800

    service-passenger-user插入用户

---
+++ service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/mapper/PassengerUserMapper.java
@@ -0,0 +1,7 @@
+package com.mashibing.servicepassengeruser.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mashibing.servicepassengeruser.dto.PassengerUser;
+
+public interface PassengerUserMapper extends BaseMapper<PassengerUser> {
+}
--- service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/UserService.java
+++ service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/UserService.java
@@ -1,19 +1,41 @@
 package com.mashibing.servicepassengeruser.service;
 
 import com.mashibing.internal.dto.ResponseResult;
-import com.mashibing.internal.request.VerificationCodeDTO;
+import com.mashibing.servicepassengeruser.dto.PassengerUser;
+import com.mashibing.servicepassengeruser.mapper.PassengerUserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class UserService {
 
+    @Autowired
+    private PassengerUserMapper passengerUserMapper;
+
     public ResponseResult loginOrRegister(String  passengerPhone){
         System.out.println("usersercice被调用了"+passengerPhone);
         //根据手机号查询用户信息
-
+        Map<String,Object> map = new HashMap<>();
+        map.put("passenger_phone",passengerPhone);
+        List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
+        System.out.println(passengerUsers.size()==0 ?"无记录":passengerUsers.get(0).getPassengerPhone());
         //判断用户信息是否存在
-
+        if(passengerUsers.size()==0){
+            PassengerUser passengerUser = new PassengerUser();
+            passengerUser.setPassengerPhone(passengerPhone);
+            passengerUser.setPassengerName("素鸡");
+            passengerUser.setPassengerGender((byte)0);
+            passengerUser.setState((byte)0);
+            LocalDateTime now = LocalDateTime.now();
+            passengerUser.setGmtCreate(now);
+            passengerUser.setGmtModified(now);
+            passengerUserMapper.insert(passengerUser);
+        }
         //如果不存在，插入用户
 
         return ResponseResult.success();
